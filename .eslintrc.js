ddc6ced00842c9682bc600a2a3019ec5c33a2bfe
Chiu Yong <chiu@vert-capital.com>
2024-01-25 20:51:19 -0300

    core: fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2022: true,
-    node: true,
+    es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-essential',
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
+    'standard-with-typescript',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+    'plugin:vuejs-accessibility/recommended',
+    '@vue/eslint-config-prettier',
   ],
+  parser: 'vue-eslint-parser',
   overrides: [
     {
       env: {
@@ -24,7 +29,95 @@ module.exports = {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
+    extraFileExtensions: ['import', 'tsdoc', 'html', 'vue'],
   },
   plugins: ['@typescript-eslint', 'vue'],
-  rules: {},
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': off,
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-generic-constructors': [
+      'error',
+      'type-annotation',
+    ],
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-confusing-void-expression': [
+      'error',
+      { ignoreArrowShorthand: true },
+    ],
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    'import/default': 'off',
+    'import/no-default-export': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          [
+            'builtin',
+            'external',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+        ],
+        pathGroups: [
+          {
+            pattern:
+              '{vue,vue-router,vuex,@/store,vue-i18n,pinia,vite,vitest,vitest/**,@vitejs/**,@vue/**,@logue/vue2-helpers}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '{vuetify,vuetify/**,@mdi/**,webfontloader}',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '{@/**}',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+        },
+        newlinesBetween: 'always',
+      },
+    ],
+    'tsdoc/syntax': 'warn',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+        },
+      },
+    ],
+    'vue/multi-word-component-names': 'warn',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      'vue-eslint-parser': ['.vue'],
+    },
+    'import/resolver': {
+      typescript: true,
+      alias: {
+        map: [
+          ['@', './src'],
+          ['~', './node_modules'],
+        ],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.vue'],
+      },
+    },
+    vite: {
+      configPath: './vite.config.ts',
+    },
+  },
 };
