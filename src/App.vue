d6815abb53c420e66d893c78c62f70441a1f3ee3
Chiu Yong <chiu@vert-capital.com>
2024-01-26 19:24:37 -0300

    feature: add movie card component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <v-app>
-    <top-nav-bar />
+    <v-app-bar app>
+      <top-nav-bar />
+    </v-app-bar>
+
     <v-main>
-      <router-view />
+      <v-container fluid>
+        <movie-card
+          :id="movie.id"
+          :image="movie.image"
+          :title="movie.title"
+          :year="movie.year"
+          :director="movie.director"
+          :runtime="movie.runtime"
+          :genre="movie.genre"
+          :plot="movie.plot"
+        ></movie-card>
+        <!-- <router-view /> -->
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -14,9 +29,34 @@ export default Vue.extend({
   name: 'App',
   components: {
     TopNavBar: () => import('@/components/layouts/TopNavBar.vue'),
+    MovieCard: () => import('@/components/base/MovieCard.vue'),
   },
   data: () => ({
     //
+    movie: {
+      id: '1',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg',
+      title: 'Star Wars: Episode IV - A New Hope',
+      year: '1977',
+      director: 'George Lucas',
+      runtime: '2h 22min',
+      genre: 'Action, Adventure, Fantasy',
+      plot: 'The Imperial Forces, under orders from cruel Darth Vader, hold Princess Leia hostage in their efforts to quell the rebellion against the Galactic Empire. Luke Skywalker and Han Solo, captain of the Millennium Falcon, work together with the companionable droid duo R2-D2 and C-3PO to rescue the beautiful princess, help the Rebel Alliance and restore freedom and justice to the Galaxy.',
+    },
   }),
 });
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+html,
+body {
+  font-size: 0.75rem;
+  font-weight: 400;
+}
+</style>
---
+++ src/components/base/MovieCard.vue
@@ -0,0 +1,387 @@
+<template>
+  <v-card :loading="loading" class="movie-card">
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+    <div class="movie-card__content">
+      <div class="movie-card__header">
+        <v-img
+          contain
+          class="movie-card__image"
+          :src="image"
+          :lazy-src="image"
+          aspect-ratio="1"
+          max-width="84"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+        <v-card-title>
+          <h1>{{ title }}</h1>
+        </v-card-title>
+        <v-card-subtitle>
+          <v-row align="center" no-gutters>
+            <v-col cols="12" justify="center">
+              <h4>{{ year }}, {{ director }}</h4>
+            </v-col>
+            <v-col cols="12" justify="center">
+              <div class="movie-card__runtime">
+                <v-chip class="minutes mr-2">
+                  <v-icon class="pr-1" small>mdi-clock</v-icon>
+                  {{ runtime }}
+                </v-chip>
+              </div>
+              <v-chip
+                v-for="(genre, index) in getGenres"
+                :key="index"
+                class="mr-2 movie-card__tags"
+                color="green"
+                text-color="white"
+                small
+              >
+                <span>{{ genre }}</span>
+              </v-chip>
+            </v-col>
+          </v-row>
+        </v-card-subtitle>
+      </div>
+      <v-card-text class="movie-card__plot">
+        <!-- <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row> -->
+        <v-row>
+          <v-col cols="auto">
+            <p class="movie-card__plot-text">
+              {{ plot }}
+            </p>
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-card-actions class="movie-card__actions">
+        <ul>
+          <li>
+            <v-btn icon @click="onClick()">
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+          </li>
+        </ul>
+      </v-card-actions>
+    </div>
+    <div class="blur-back" :style="backgroundStyle"></div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {
+  VBtn,
+  VCard,
+  VCardActions,
+  VCardSubtitle,
+  VCardText,
+  VCardTitle,
+  VImg,
+  VRating,
+} from 'vuetify/lib';
+
+export default Vue.extend({
+  components: {
+    VCard,
+    VImg,
+    VCardTitle,
+    VCardSubtitle,
+    VCardText,
+    VCardActions,
+    VBtn,
+    VRating,
+  },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: false,
+    },
+    year: {
+      type: String,
+      required: false,
+    },
+    director: {
+      type: String,
+      required: false,
+    },
+    runtime: {
+      type: String,
+      required: false,
+    },
+    genre: {
+      type: String,
+      required: false,
+    },
+    plot: {
+      type: String,
+      required: false,
+    },
+  },
+  computed: {
+    getGenres(): string[] {
+      return this.genre.split(',').map(genre => genre.trim());
+    },
+    backgroundStyle(): Record<string, string> {
+      return {
+        background: `url(${this.image})`,
+      };
+    },
+  },
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+::v-deep.movie-card {
+  position: relative;
+  display: block;
+  width: 100%;
+  max-width: 800px;
+  height: 450px;
+  margin: 6rem auto;
+  overflow: hidden;
+  border-radius: 10px !important;
+  transition: all 0.4s;
+  box-shadow: 0px 0px 150px -45px rgba(19, 160, 134, 0.6) !important;
+  &:hover {
+    box-shadow: 0px 0px 120px -55px rgba(19, 160, 134, 0.6);
+    transform: scale(1.02);
+    transition: all 0.4s;
+  }
+  &__content {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-blend-mode: multiply;
+    z-index: 2;
+    border-radius: 10px;
+  }
+
+  &__header {
+    position: relative;
+    padding: 25px;
+    // height: 40%;
+    height: fit-content;
+    .v-card__title {
+      padding-top: 0;
+      padding-left: 0;
+      padding-right: 0;
+    }
+    .v-card__subtitle {
+      font-size: 0.813rem;
+    }
+    h1 {
+      font-size: 1.5rem;
+      color: #fff;
+      font-weight: 800;
+      word-break: break-word;
+    }
+    h4 {
+      color: #9ac7fa;
+      font-weight: 400;
+      margin-bottom: 10px;
+      margin-top: 0.25rem;
+    }
+    .minutes {
+      display: inline-block;
+      color: #fff;
+      padding: 5px;
+      border-radius: 5px;
+      border: 1px solid rgba(255, 255, 255, 0.13);
+      font-size: 0.813rem;
+    }
+    &__tags {
+      display: inline-block;
+      span {
+        font-size: 0.75rem;
+      }
+    }
+  }
+
+  &__image {
+    position: relative;
+    float: left;
+    margin-right: 20px;
+    height: 120px;
+    box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.5);
+  }
+  &__runtime {
+    margin-bottom: 0.625rem;
+    span {
+      font-size: 0.75rem;
+    }
+  }
+  &__plot {
+    padding: 25px;
+    height: 50%;
+  }
+
+  &__plot-text {
+    color: #cfd6e1;
+    font-size: 0.75rem;
+  }
+
+  &__actions {
+    height: 10%;
+    padding-left: 15px;
+    padding-bottom: 20px;
+    ul {
+      list-style: none;
+      padding: 0;
+      li {
+        display: inline-block;
+        color: rgba(255, 255, 255, 0.4);
+        transition: color 0.3s;
+        transition-delay: 0.15s;
+        margin: 0 10px;
+        &:hover {
+          transition: color 0.3s;
+          color: rgba(255, 255, 255, 0.8);
+        }
+        i {
+          font-size: 19px;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+  .blur-back {
+    position: absolute;
+    top: 0;
+    z-index: 1;
+    height: 100%;
+    right: 0;
+    background-size: cover !important;
+    border-radius: 11px;
+    background: url('https://fsmedia.imgix.net/cd/c9/5e/ba/4817/4d9a/93f0/c776ec32ecbc/lara-crofts-neck-looks-unnatural-in-the-new-poster-for-tomb-raider.png');
+  }
+}
+
+/* SM, MD, LG, XL */
+@media screen and (min-width: 769px) {
+  .movie-card {
+    &__content {
+      background: linear-gradient(to right, #0d0d0c 50%, transparent 100%);
+    }
+    &__header {
+      width: 60%;
+      height: 48%;
+      .v-card__subtitle {
+        padding: 0;
+      }
+    }
+    &__image {
+      height: 100%;
+    }
+    &__runtime {
+      margin-bottom: 0.625rem;
+    }
+    &__plot {
+      width: 50%;
+      padding-top: 0;
+      padding-bottom: 0;
+      height: fit-content;
+    }
+    &__actions {
+      padding: 0 0 0 25px;
+      ul {
+        li {
+          margin: 0;
+        }
+      }
+    }
+    .blur-back {
+      width: 80%;
+      background-position: -100% 10% !important;
+    }
+  }
+}
+/* Mobile */
+@media screen and (max-width: 768px) {
+  ::v-deep.movie-card {
+    width: 95%;
+    margin: 70px auto;
+    min-height: 350px;
+    height: auto;
+    &__content {
+      background: linear-gradient(
+        to top,
+        rgb(20, 20, 19) 50%,
+        transparent 100%
+      );
+      display: inline-grid;
+    }
+    .v-card__title {
+      padding-top: 1rem;
+      font-size: 0.875rem;
+    }
+    .v-card__subtitle {
+      padding: 0;
+      font-size: 0.813rem;
+    }
+    &__image {
+      float: none;
+      margin: auto;
+    }
+    &__header {
+      width: 100%;
+      margin-top: 85px;
+    }
+    &__plot {
+      width: 100%;
+      padding-top: 0 !important;
+    }
+    &__actions {
+      display: flex;
+      justify-content: center;
+      padding: 0;
+    }
+    .blur-back {
+      width: 100%;
+      background-position: 50% 50% !important;
+      height: 50%;
+    }
+  }
+}
+</style>
