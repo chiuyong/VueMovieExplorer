f582ada2f7a8a69c8c0a1322c85b830c82773f7e
Chiu Yong <chiu@vert-capital.com>
2024-01-26 01:20:30 -0300

    core: add mdi integration to vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import pt from 'vuetify/lib/locale/pt';
@@ -8,5 +9,8 @@ export default new Vuetify({
   lang: {
     locales: { pt },
     current: 'pt',
+  },
+  icons: {
+    iconfont: 'mdi',
   },
 });
